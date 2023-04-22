90153f3db6cec95c870a0e0e02d102391ddacba9
despan5 <des19002@byui.edu>
2023-04-22 11:23:00 -0400

    Prep 1 and 2 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+
+        int percent = int.Parse(grade);
+
+        string lettergrade = "";
+
+        if (percent >= 90)
+        {
+            lettergrade = "A";
+        }
+
+        else if (percent >= 80)
+        {
+            lettergrade = "B";
+        }
+
+        else if (percent >= 70)
+        {
+            lettergrade = "C";
+        }
+
+        else if (percent >= 60)
+        {
+            lettergrade = "D";
+        }
+
+        else
+        {
+            lettergrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {lettergrade}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("Better luck next time :(");
+        }
     }
 }
