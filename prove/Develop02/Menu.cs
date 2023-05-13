a627c45f6b53afd8a82ebf65babb252a9fec172e
Matt Despain <mattdespainphotovideo@gmail.com>
2023-05-13 10:09:54 -0400

    Develope02 rough draft

---
+++ prove/Develop02/Menu.cs
@@ -0,0 +1,18 @@
+using System;
+  
+public class Menu {  
+  public int Show()
+
+    {  
+        Console.WriteLine("Please select an option:");  
+        Console.WriteLine("1) Write");  
+        Console.WriteLine("2) Display");  
+        Console.WriteLine("3) Save");  
+        Console.WriteLine("4) Load");  
+        Console.WriteLine("5) Quit")
+        int Choice = Console.ReadLine();
+        return Choice
+        
+
+    }  
+    }  
