c2473cb6166c08a8a0860adf33623d632eda312b
despan5 <des19002@byui.edu>
2023-05-27 23:42:39 -0400

    memorization program complete with classes

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,14 @@
-using System;
+using Develope03;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        string verse = "Behold, I am Jesus Christ, whom the prophets testified shall come into the world. And behold, I am the light and the life of the world; and I have drunk out of that bitter cup which the Father hath given me, and have glorified the Father in taking upon me the sins of the world, in the which I have suffered the will of the Father in all things from the beginning.";
+        Scripture scripture = new Scripture();
+
+        Console.Clear();
+
+        Console.ReadLine();
     }
 }
